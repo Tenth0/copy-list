602050503b75ffee426c7efbbf814cdac59e5168
Tenth0 <105034393+Tenth0@users.noreply.github.com>
2023-05-19 14:42:26 +0900

    テキスト投稿フォームのモーダルを作成

--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/insert', [App\Http\Controllers\CopyTextController::class, 'insertText'])->name('insert_text');
+Route::post('/insert', [App\Http\Controllers\CopyTextController::class, 'insertText'])->name('insert_text');

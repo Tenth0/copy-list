888605cd0420df4cc7fdffcd4d788cd88ca4a073
Tenth0 <105034393+Tenth0@users.noreply.github.com>
2023-05-23 11:55:32 +0900

    fix:登録できないエラーの修正

--- app/Http/Controllers/Text/CopyTextController.php
+++ app/Http/Controllers/Text/CopyTextController.php
@@ -1,55 +1,55 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Text;
 
+use App\Http\Controllers\Controller;
 use App\Http\Requests\CreateTextRequest;
-use App\Models\Category;
+use App\Models\UserCategory;
 use App\Models\Text;
 use App\Models\TextCategory;
-use Illuminate\Auth\EloquentUserProvider;
-use Illuminate\Contracts\View\View;
-use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\Auth;
 
 class CopyTextController extends Controller
 {
-    private $categoryModel;
+    private $userCategoryModel;
     private $textModel;
     private $textCategoryModel;
 
     public function __construct(
-        Category $categoryModel,
+        UserCategory $userCategoryModel,
         Text $textModel,
-        TextCategory $textCategoryModel
+        TextCategory $textCategoryModel,
     )
     {
-        $this->categoryModel = $categoryModel;
+        $this->userCategoryModel = $userCategoryModel;
         $this->textModel = $textModel;
         $this->textCategoryModel = $textCategoryModel;
+        $this->middleware('auth');
     }
 
-    public function insertText(CreateTextRequest $request)
+    public function insertText(CreateTextRequest $request)//: RedirectResponse
     {
-        $user_id = auth()->user()->id;
-
-        $category_id_first = isset($request['category_name_first']) ? $this->categoryModel->getCategoryId($request['category_name_first']) : null;
-        $category_id_second = isset($request['category_name_second']) ? $this->categoryModel->getCategoryId($request['category_name_second']) : null;
-        $category_id_third = isset($request['category_name_third']) ? $this->categoryModel->getCategoryId($request['category_name_third']) : null;
-        $categoryIds = [
-           'category_id_first' => $category_id_first,
-           'category_id_second' => $category_id_second,
-           'category_id_third' => $category_id_third 
-        ];
-
+        $userId = (int) auth()->id();
         $textData = [
-            'user_id' => $user_id,
+            'user_id' => $userId,
             'text_content' => $request['text_content'],
         ];
         
         $this->textModel->saveText($textData);
-        $this->textCategoryModel->saveCategory($categoryIds);
 
+        $textId = $this->textModel->getLastInsertId($userId)->id;
+        $categoryIdFirst = isset($request['category_name_first'])   ? $this->userCategoryModel->getCategoryId($request['category_name_first']) : null;
+        $categoryIdSecond = isset($request['category_name_second']) ? $this->userCategoryModel->getCategoryId($request['category_name_second']) : null;
+        $categoryIdThird = isset($request['category_name_third'])   ? $this->userCategoryModel->getCategoryId($request['category_name_third']) : null;
+        $textCategoryData = [
+            'text_id' => $textId,
+            'category_id_first' => $categoryIdFirst,
+            'category_id_second' => $categoryIdSecond,
+           'category_id_third' => $categoryIdThird 
+        ];        
+        $this->textCategoryModel->saveCategory($textCategoryData);
         return redirect()->route('home')->with('insert-success', '追加が完了しました');
     }
 
